89800c3497612ec35f3ff5ba97ba75a8522f6906
Alenka Sharipova <alenkasharipova@MacBookAir.Dlink>
2025-06-14 11:53:05 +0300

    update

--- apigateway/src/main/java/com/shop/gateway/GatewayApplication.java
+++ apigateway/src/main/java/com/shop/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.shop.gateway;
+package com.shop.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
